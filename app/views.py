c9d16f50d88fe06dffe4f32f20f73d12f52be9e2
ineqwij <michaelji111208@yahoo.com>
2019-03-25 19:15:46 +0000

    init

--- app/views.py
+++ app/views.py
@@ -20,11 +20,11 @@ records = [
 def index():
     return "<h1>----hello flask!----</h1>"
 
-@app.route('/records/', methods=['GET'])
+@app.route('/records', methods=['GET'])
 def getRec():
     return jsonify({'records': records})
 
-@app.route('/records/', methods=['POST'])
+@app.route('/records', methods=['POST'])
 def createRec():
     if not request.json or not 'title' in request.json:
         return make_response(jsonify({'-ERROR-': 'NEED TITLE'}), 400)
@@ -41,6 +41,26 @@ def createRec():
 def getTtl():
     response = [item['title'] for item in records]
     return jsonify(response)
+
+@app.route('/records/<ttlName>', methods=['DELETE'])
+def deleteTask(ttlName):
+    matching = [item for item in records if item['title'] == ttlName]
+    if len(matching) == 0:
+        return make_response(jsonify({'-ERROR-': 'NOT FOUND'}), 404)
+    records.remove(matching[0])
+    return jsonify({'sucess', True})
+
+@app.route('/records/<ttlName>', methods=['PUT'])
+def updateTask(ttlName):
+    matching = [item for item in records if item['title'] == ttlName]
+    if len(matching) == 0:
+        return make_response(jsonify({'-ERROR-': 'NOT FOUND'}), 404)
+    if not request.json:
+        return make_response(jsonify({'-ERROR-': 'FORMAT ERR'}), 400)
+    matching[0]['title'] = request.json.get('title', matching[0]['title'])
+    matching[0]['description'] = request.json.get('description', matching[0]['description'])
+    matching[0]['done'] = request.json.get('done', matching[0]['done'])
+    return jsonify({'task':matching[0]})
 
 @app.errorhandler(404)
 def not_found(error):
